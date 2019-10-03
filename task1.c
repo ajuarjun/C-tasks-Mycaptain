a8f8a056ca621f5125980cb093b43def5245ebe5
SSidharth1505 <43806972+SSidharth1505@users.noreply.github.com>
2019-10-03 20:39:05 +0530

    Update task1.c
    Pls add these extra features too

--- task1.c
+++ task1.c
@@ -3,6 +3,7 @@ main()
 {int age;
  char name[10];
  long int phone;
+ char city[30];
  
  printf("\n Enter name:");
  scanf("%s",name);
@@ -10,10 +11,13 @@ main()
  scanf("%d",&age);
  printf("\n Enter telephone number:");
  scanf("%d",&phone);
+ printf("\n Enter the city in which you are residing!:");
+ gets(city);
  
  printf("\n The details are:");
  printf("\n Name: %s",name);
  printf("\n Age: %d",age);
  printf("\n Phone number: ");
  printf("%d",phone);
+ printf("\n City: %s",city);
 }
